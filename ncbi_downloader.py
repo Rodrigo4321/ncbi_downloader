381adb92ebe1094ac8468e6a31cc633f54238422
kikargentkiko <62952046+kikargentkiko@users.noreply.github.com>
2020-03-31 17:12:15 +0100

    Update ncbi_downloader.py

--- ncbi_downloader.py
+++ ncbi_downloader.py
@@ -3,17 +3,16 @@
 import sys
 from Bio import Entrez
 
-base_dados = sys.argv[1]
-search_term = sys.argv[2]
+base_dados = sys.argv[1] #Atribuição os parametro da linha de comandos a variáveis
+search_term = sys.argv[2] 
 
 
 file = open("sequences.fasta", "w")
-sys.stdout = file
 
-search = Entrez.esearch(db=base_dados, term=search_term, usehistory="y")
+search = Entrez.esearch(db=base_dados, term=search_term, usehistory="y") #O search é uma variável que vai publicar os resultados de uma pesquisa no histórico do servidor
 search_results  = Entrez.read(search)
 
-fetch = Entrez.efetch(db=base_dados, usehistory="y", webenv=search_results["WebEnv"], query_key=search_results["QueryKey"], rettype="fasta")
+fetch = Entrez.efetch(db=base_dados, usehistory="y", webenv=search_results["WebEnv"], query_key=search_results["QueryKey"], rettype="fasta") #O efetch retorna registos de dados formatados para um conjunto de UIDs armazenados no histórico do servidor Entrez
 
 sequences = fetch.read()
 
