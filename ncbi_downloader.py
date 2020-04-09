abbd5e0437329ef03b1a287bcde6dcfeffcee8ea
unknown <201800039@estudantes.ips.pt>
2020-04-09 01:00:49 +0100

    script correction

--- ncbi_downloader.py
+++ ncbi_downloader.py
@@ -6,16 +6,14 @@ from Bio import Entrez
 base_dados = sys.argv[1] #Atribuição dos parametro da linha de comandos a variáveis
 search_term = sys.argv[2] 
 
-
-file = open("sequences.fasta", "w")
-
 search = Entrez.esearch(db=base_dados, term=search_term, usehistory="y") #O search é uma variável que vai publicar os resultados de uma pesquisa no histórico do servidor
 search_results  = Entrez.read(search)
 
 fetch = Entrez.efetch(db=base_dados, usehistory="y", webenv=search_results["WebEnv"], query_key=search_results["QueryKey"], rettype="fasta") #O efetch retorna registos de dados formatados para um conjunto de UIDs armazenados no histórico do servidor Entrez
 
 sequences = fetch.read()
-
 fetch.close()
 
-file.write(sequences)
+sys.stdout.write(sequences)
+sys.stdout.flush()
+
